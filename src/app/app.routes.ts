b810fe9691e344359ef0e3cfd38da810d8093515
Nelson Valverde La Torre <nelsonvalverdelt@aysconsultores.pe>
2024-08-27 01:30:15 -0500

    delete base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,4 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './list/list.component';
-import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: ListComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  }
 ];
--- src/app/shared/models/order.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Order{
-  id: string,
-  productCode: string,
-  date: string,
-  amount: number,
-  quantity: number,
-  customer: string,
-  status: 'PENDING' | 'DELIVERED' | 'RETURNED' | 'CANCELLED',
-}
